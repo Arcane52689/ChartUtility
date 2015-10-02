285bb578bf480f0f93ba90ea77e75fec5b34cf36
Arcane52689 <tjenkins52689@gmail.com>
2015-10-02 14:58:41 -0400

    Bar graph is now funcitonal, if not optimal

--- bar_graph.js
+++ bar_graph.js
@@ -12,6 +12,7 @@
     this.verticalSpaces = options.verticalSpaces || this.setSpacing()
     this.horizontalLines = options.horizontalLines || false;
     this.horizontalSpacing = options.horizontalSpacing || this.estimateHorizontalSpacing();
+    this.barWidth = options.barWidth || this.estimateBarWidth();
   }
 
   BarGraph.prototype.setup = function(canvasId) {
@@ -42,13 +43,19 @@
     return Math.max(height, max);
   }
 
-  Bargraph.prototype.estimateHorizontalSpacing = function() {
-    var increment = Math.floor(this.width/this.objs.length);
-    var spacing = [this.origin[0]]
-    while (spacing[spacing.length  -1] < this.width) {
+  BarGraph.prototype.estimateHorizontalSpacing = function() {
+    var increment = Math.floor(this.width/this.objs.length + 1);
+    var spacing = [this.origin[0] + increment ]
+    while (spacing[spacing.length  -1] <= this.width) {
       spacing.push(spacing[spacing.length - 1] + increment );
     }
     return spacing;
+  }
+
+  BarGraph.prototype.estimateBarWidth = function() {
+    var standard = this.width / 10;
+    var estimated = this.width / this.objs.length;
+    return Math.min(standard, estimated);
   }
 
   BarGraph.prototype.calculateOrigin = function() {
@@ -73,11 +80,11 @@
   }
 
   BarGraph.prototype.markAxes = function() {
-    this.markVerticalAxes();
-    this.markHorizontalAxes();
+    this.markVerticalAxis();
+    this.markHorizontalAxis();
   }
 
-  BarGraph.prototype.markVerticalAxes = function() {
+  BarGraph.prototype.markVerticalAxis = function() {
 
     if (this.horizontalLines) {
       end[0] = end[0] + this.width;
@@ -85,7 +92,7 @@
     var markAxis = function(space) {
       var start = [this.origin[0] - 25, this.origin[1] - this.percentOf(space)];
       var end = [this.origin[0], this.origin[1] - this.percentOf(space)]
-      debugger
+
       this.ctx.moveTo(start[0], start[1])
       this.ctx.lineTo(end[0], end[1])
       this.ctx.fillText(space + "", 0, start[1])
@@ -97,6 +104,21 @@
 
   }
 
+  BarGraph.prototype.markHorizontalAxis = function() {
+
+    var drawBar = function(obj, space) {
+      var height = this.percentOf(obj[this.value]);
+      this.ctx.beginPath();
+      this.ctx.fillStyle = obj.color;
+      debugger
+      this.ctx.rect(space, this.origin[1], this.barWidth, height *-1 )
+      this.ctx.fillText(obj[this.key],space, this.canvas.height * .95, this.barWidth);
+      this.ctx.fill();
+    }.bind(this)
+    for (var i = 0; i < this.objs.length; i++) {
+      drawBar(this.objs[i], this.horizontalSpacing[i])
+    }
+  }
 
 
   BarGraph.prototype.execute = function() {
